f895124a027741dafc088d3a9fceb35da9cef1d5
Feist Josselin <josselin.feist@gmail.com>
2024-02-15 15:25:11 +0100

    Modernize install (PEP 621)

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
-"""
-Install script for crytic-exploit package
-"""
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
-    name="test_generator",
-    description="A tool for automatically generating unit tests from Echidna and Medusa reproducers",
-    url="https://github.com/crytic/test-generator",
-    author="Trail of Bits",
-    version="0.0.1",
-    packages=find_packages(exclude=["tests"]),
-    python_requires=">=3.7",
-    install_requires=[
-        "colorama",
-        "crytic_compile",
-        "eth_utils",
-        "slither_analyzer",
-        "web3",
-        "jinja2",
-    ],
-    entry_points={"console_scripts": ["test-generator = test_generator.main:main"]},
-)
+setuptools.setup()
