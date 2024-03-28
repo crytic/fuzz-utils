1d1db7b863cb0a683c2ce80402efc219ec7b1530
tuturu-tech <elvis.skozdopolj@trailofbits.com>
2024-03-28 18:11:59 +0100

    format

--- fuzz_utils/utils/encoding.py
+++ fuzz_utils/utils/encoding.py
@@ -2,43 +2,43 @@
 import re
 
 ascii_escape_map = {
-    "\\NUL": b'\x00',  # Null character
-    "\\SOH": b'\x01',
-    "\\STX": b'\x02',
-    "\\ETX": b'\x03',
-    "\\EOT": b'\x04',
-    "\\ENQ": b'\x05',
-    "\\ACK": b'\x06',
-    "\\FF": b'\x0c',
-    "\\CR": b'\x0d',
-    "\\SO": b'\x0e',
-    "\\SI": b'\x0f',
-    "\\DLE": b'\x10',
-    "\\DC1": b'\x11',
-    "\\DC2": b'\x12',
-    "\\DC3": b'\x13',
-    "\\DC4": b'\x14',
-    "\\NAK": b'\x15',
-    "\\SYN": b'\x16',
-    "\\ETB": b'\x17',
-    "\\CAN": b'\x18',
-    "\\EM": b'\x19',
-    "\\SUB": b'\x1a',
-    "\\ESC": b'\x1b',
-    "\\FS": b'\x1c',
-    "\\GS": b'\x1d',
-    "\\RS": b'\x1e',
-    "\\US": b'\x1f',
-    "\\SP": b'\x20',
-    "\\DEL": b'\x7f',
-    "\\0": b'\x00',
-    "\\a": b'\x07',  # Alert
-    "\\b": b'\x08',  # Backspace
-    "\\f": b'\x0c',
-    "\\n": b'\x0a',  # New line
-    "\\r": b'\x0d',
-    "\\t": b'\x09',  # Horizontal Tab
-    "\\v": b'\x0b',  # Vertical Tab
+    "\\NUL": b"\x00",  # Null character
+    "\\SOH": b"\x01",
+    "\\STX": b"\x02",
+    "\\ETX": b"\x03",
+    "\\EOT": b"\x04",
+    "\\ENQ": b"\x05",
+    "\\ACK": b"\x06",
+    "\\FF": b"\x0c",
+    "\\CR": b"\x0d",
+    "\\SO": b"\x0e",
+    "\\SI": b"\x0f",
+    "\\DLE": b"\x10",
+    "\\DC1": b"\x11",
+    "\\DC2": b"\x12",
+    "\\DC3": b"\x13",
+    "\\DC4": b"\x14",
+    "\\NAK": b"\x15",
+    "\\SYN": b"\x16",
+    "\\ETB": b"\x17",
+    "\\CAN": b"\x18",
+    "\\EM": b"\x19",
+    "\\SUB": b"\x1a",
+    "\\ESC": b"\x1b",
+    "\\FS": b"\x1c",
+    "\\GS": b"\x1d",
+    "\\RS": b"\x1e",
+    "\\US": b"\x1f",
+    "\\SP": b"\x20",
+    "\\DEL": b"\x7f",
+    "\\0": b"\x00",
+    "\\a": b"\x07",  # Alert
+    "\\b": b"\x08",  # Backspace
+    "\\f": b"\x0c",
+    "\\n": b"\x0a",  # New line
+    "\\r": b"\x0d",
+    "\\t": b"\x09",  # Horizontal Tab
+    "\\v": b"\x0b",  # Vertical Tab
 }
 
 
@@ -49,13 +49,13 @@ def parse_echidna_byte_string(s: str, isBytes: bool) -> str:
     result_bytes = bytearray()
 
     # Regular expression to match decimal values like \160
-    decimal_pattern = re.compile(r'\\(\d{1,3})')
+    decimal_pattern = re.compile(r"\\(\d{1,3})")
 
     # Iterator over the string
     i = 0
     while i < len(s):
         # Check for escape sequence
-        if s[i] == '\\':
+        if s[i] == "\\":
             matched = False
             # Check for known escape sequences
             for seq, byte in ascii_escape_map.items():
@@ -84,6 +84,7 @@ def parse_echidna_byte_string(s: str, isBytes: bool) -> str:
     if not isBytes:
         return byte_to_escape_sequence(bytes(result_bytes))
     return result_bytes.hex()
+
 
 def byte_to_escape_sequence(byte_data: bytes) -> str:
     """Generates unicode escaped string from bytes"""
